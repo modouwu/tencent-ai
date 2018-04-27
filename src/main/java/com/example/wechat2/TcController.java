ecd73fccc50a62af96959445e478c96ad5421de3
yeyunlong <yeyunlong@gozap.com>
2018-04-27 14:54:45 +0800

    优化

--- src/main/java/com/example/wechat2/TcController.java
+++ src/main/java/com/example/wechat2/TcController.java
@@ -1,52 +1,63 @@
 package com.example.wechat2;
 
-import org.apache.http.HttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import sun.misc.BASE64Decoder;
-import sun.misc.BASE64Encoder;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.URLEncoder;
-import java.util.Base64;
+
+import javax.annotation.Resource;
+
 import java.util.HashMap;
 import java.util.Map;
 
 @Controller
 public class TcController {
 
+    @Resource
+    ImageInfoService imageInfoService;
+
+    @Autowired
+    Environment env;
 
     @RequestMapping("/img")
     @ResponseBody
     public String getImageInfo() throws Exception {
-        String pathName="/home/yeyunlong/Downloads/aaa.jpg";
-        int type=0;//识别类型，0-行驶证识别，1-驾驶证识别
         Map<String,String> mapParams=new HashMap<>();
-        mapParams.put("app_id",String.valueOf(ImageInfoService.app_id));
+        mapParams.put("app_id",env.getProperty("app_id"));
         mapParams.put("time_stamp",Long.toString(System.currentTimeMillis()/1000));
         mapParams.put("nonce_str",Long.toString(System.currentTimeMillis()));
-        BASE64Encoder encoder = new BASE64Encoder();
-        InputStream inputStream=new FileInputStream(pathName);
-        byte[] bytes=new byte[inputStream.available()];
-        inputStream.read(bytes);
-        inputStream.close();
-        String image=encoder.encode(bytes).replace("\n","");
-//        image =  URLEncoder.encode(image, "UTF-8");
+        String image=imageInfoService.base64Encode(env.getProperty("pathName"));
         mapParams.put("image", image);
-        mapParams.put("type",type+"");
-        String sign=ImageInfoService.createSign(mapParams,ImageInfoService.key);
-        System.out.println("MD5:"+sign);
-        //String sign2=ImageInfoService.getSignature(mapParams);
+        mapParams.put("type",env.getProperty("type"));
+        String sign=imageInfoService.createSign(mapParams,env.getProperty("key"));
         mapParams.put("sign",sign);
-        //mapParams.remove("app_key");
-        //String result=ImageInfoService.postInfo(ImageInfoService.url,mapParams);
-        String result=ImageInfoService.doPostTencentAI(ImageInfoService.url,mapParams);
-        //String result=HttpUtil.doPostSSL(ImageInfoService.url,mapParams);
-        //String result=HttpUtil.sendPost(ImageInfoService.url,mapParams,null);
+        String result=imageInfoService.doPostUrl(env.getProperty("url"),mapParams);
+        return result;
+    }
+
+    @RequestMapping("/access_token")
+    @ResponseBody
+    public String getAccessToken() throws Exception {
+        Map<String,String> map=new HashMap<>();
+        map.put("grant_type",env.getProperty("grant_type"));
+        map.put("appid",env.getProperty("appID"));
+        map.put("secret",env.getProperty("appsecret"));
+        String result=imageInfoService.doGetUrl(env.getProperty("accesstokenurl"),map);
+        return result;
+    }
+
+    @RequestMapping("/uerinfo")
+    @ResponseBody
+    public String getUserInfo(@RequestParam(name = "openid") String openid) throws Exception {
+        Map<String,String> map=new HashMap<>();
+        map.put("access_token","");
+        map.put("openid",openid);
+        map.put("lang",env.getProperty("lang"));
+        String result=imageInfoService.doGetUrl(env.getProperty("accesstokenurl"),map);
         return result;
     }
 }
--- src/test/java/com/example/wechat2/ITController.java
+++ src/test/java/com/example/wechat2/ITController.java
@@ -5,11 +5,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
 import sun.misc.BASE64Decoder;
 import sun.misc.BASE64Encoder;
 
+import javax.annotation.Resource;
 import java.io.*;
 import java.net.InetAddress;
 import java.net.URLEncoder;
@@ -23,6 +25,12 @@ import java.util.Map;
 @Slf4j
 public class ITController {
 
+    @Resource
+    ImageInfoService imageInfoService;
+
+    @Autowired
+    Environment env;
+
     @Test
     public void testbase64() throws IOException {
         String pathName="/home/yeyunlong/Desktop/aaa";
@@ -35,19 +43,6 @@ public class ITController {
         System.out.println(image);
     }
 
-    /*@Test
-    public void appSignBaseAI4FlowersDetect(Integer appId,
-                                                      String nonce_str,String image,Integer scene) throws Exception {
-        String time_stamp = System.currentTimeMillis()/1000 + "";
-        String plain_text = "app_id=" + URLEncoder.encode(appId.toString(),"UTF-8")
-                + "&image=" + URLEncoder.encode(image,"UTF-8")
-                +"&nonce_str=" + URLEncoder.encode(nonce_str,"UTF-8")
-                +"&scene=" + URLEncoder.encode(scene.toString(),"UTF-8")
-                + "&time_stamp=" + URLEncoder.encode(time_stamp,"UTF-8");
-        String plain_text_encode = plain_text+"&app_key="+TencentAPI.APP_KEY_AI;
-        String sign = MD5.getMD5(plain_text_encode);
-    }*/
-
     @Test
     public void testmd5() throws Exception {
         Map<String,String> mapParams=new HashMap<>();
@@ -56,13 +51,19 @@ public class ITController {
         mapParams.put("nonce_str",System.currentTimeMillis()+"");
         mapParams.put("key1","腾讯AI开放平台");
         mapParams.put("key2","示例仅供参考");
-        String sign=ImageInfoService.createSign(mapParams,"B1Pej0A9qdZnTN7E");
+        String sign=imageInfoService.createSign(mapParams,"B1Pej0A9qdZnTN7E");
         String md5Str ="BE918C28827E0783D1E5F8E6D7C37A61";
         System.out.println("sign.equals(md5Str)="+sign.equals(md5Str));
         System.out.println("得到md5为"+sign);
-        mapParams.put("sign",sign);
-//        String result=ImageInfoService.doPostTencentAI("https://api.ai.qq.com/path/to/api",mapParams);
-        String result=ImageInfoService.doPostTencentAI(ImageInfoService.url,mapParams);
-        System.out.println("得到result为"+result);
+    }
+
+    @Test
+    public void testReplace(){
+        String s = "my.test.txt";
+        System.out.println(s.replace(".", "#"));
+        System.out.println(s.replaceAll(".", "#"));
+        System.out.println(s.replaceAll("\\.", "#"));
+        System.out.println(s.replaceFirst(".", "#"));
+
     }
 }
